60c75c47b2f2d38f505eb46c1a7bb1afd2e1caac
lukas <lukas.koch-schulte@gmx.de>
2023-08-19 18:04:03 +0200

    intial push

--- src/main/java/com/lukas/zoohandlungfx/ZoohandlungController.java
+++ src/main/java/com/lukas/zoohandlungfx/ZoohandlungController.java
@@ -14,13 +14,13 @@ import java.util.ResourceBundle;
 public class ZoohandlungController implements Initializable {
 
     @FXML
-    private TreeView<String> tvZoo;
+    private TreeView<String> treeZoo;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         TreeItem<String> RootNode = new TreeItem<>("Zoohandlungen");
         RootNode.setExpanded(true);
-        tvZoo.setRoot(RootNode);
+        treeZoo.setRoot(RootNode);
 
         ObservableList<TreeItem<String>> ChildList = RootNode.getChildren();
         for (int i = 1; i < 11; i++) {
@@ -32,5 +32,41 @@ public class ZoohandlungController implements Initializable {
             TreeItem<String> item3 = new TreeItem<>("Mangement");
             subChilds.addAll(item1, item2, item3);
         }
+    }
+
+    public void openAll() {     //Menu Bar Management / Alle Öffnen
+        System.out.println("Alle Zoohandlungen wurden Geöffnet");
+    }
+
+    public void closeAll() {    //Menu Bar Management / Alle Schließen
+        System.out.println("Alle Zoohandlungen wurden geschlossen");
+    }
+
+    public void openTimes() {   //Menu Bar Management / Öffnungzeiten
+        System.out.println("Öffnungszeiten ändern");
+    }
+
+    public void addZoo() {      //Menu Bar Zoohandlungen / Hinzufügen
+        System.out.println("Zoohandlung hinzufügen");
+    }
+
+    public void removeZoo() {   //Menu Bar Zoohandlungen / Entfernen
+        System.out.println("Zoohandlung entfern");
+    }
+
+    public void toggleAutoOpening() {   //Menu Bar Einstellungen / Automatische Öffnungszeiten
+        System.out.println("Toggle automatische Öffnungszeiten");
+    }
+
+    public void balance() {     //Menu Bar Bank / Kontostand
+        System.out.println("Kontostand anzeigen");
+    }
+
+    public void addBalance() {  //Menu Bar Bank / Geld Hinzufügen
+        System.out.println("Geld hinzufügen");
+    }
+
+    public void removeBalance() {
+        System.out.println("Geld entfernen");
     }
 }
