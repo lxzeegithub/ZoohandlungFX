e1864e140242c29f2563a1ea720ed73eca08a27d
lukas <lukas.koch-schulte@gmx.de>
2023-08-24 22:00:10 +0200

    intital push

--- src/main/java/com/lukas/zoohandlungfx/ZoohandlungController.java
+++ src/main/java/com/lukas/zoohandlungfx/ZoohandlungController.java
@@ -2,9 +2,12 @@ package com.lukas.zoohandlungfx;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.text.Font;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -44,19 +47,53 @@ public class ZoohandlungController implements Initializable {
         tree.getRoot().getChildren().add(zoohandlung);
     }
 
+    private Label createLabel(String text, Font font, int x, int y) {
+        Label label = new Label(text);
+        label.setFont(font);
+        label.setTranslateX(x);
+        label.setTranslateY(y);
+        return label;
+    }
+
     private void addStack() {
         StackPane pane = new StackPane();
         TabPane tiereTab = new TabPane();
         TabPane pflegerTab = new TabPane();
         AnchorPane settingsPane = new AnchorPane();
+
+        //Neues Tier
         AnchorPane neuesTierPane = new AnchorPane();
+        Label titelNeuesTier = new Label("Neues Tier");
+        titelNeuesTier.setFont(new Font("Arial", 24));
+        titelNeuesTier.setMaxWidth(Double.MAX_VALUE);
+        AnchorPane.setLeftAnchor(titelNeuesTier, 0.0);
+        AnchorPane.setRightAnchor(titelNeuesTier, 0.0);
+        titelNeuesTier.setAlignment(Pos.CENTER);
+        titelNeuesTier.setTranslateY(10);
+        Label nameTierLabel = createLabel("Name", new Font("Arial", 15), 120, 60);
+        Label alterTierLabel = createLabel("Alter", new Font("Arial", 15), 120, 90);
+        Label tierArtLabel = createLabel("Tierart", new Font("Arial", 15), 120, 120);
+        Label rasseTierLabel = createLabel("Rasse", new Font("Arial", 15), 120, 150);
+        Label preisTierLabel = createLabel("Preis", new Font("Arial", 15), 120, 180);
+        neuesTierPane.getChildren().addAll(
+                titelNeuesTier,
+                nameTierLabel,
+                alterTierLabel,
+                tierArtLabel,
+                rasseTierLabel,
+                preisTierLabel);
 
-        neuesTierPane.getChildren().add(new Button("Meister"));
-        //Add Object to Pane
-
+        //Tiere
         AnchorPane tierePane = new AnchorPane();
+
+
+        //Neuer Pfleger
         AnchorPane neuerPflegerPane = new AnchorPane();
+
+
+        //Pfleger
         AnchorPane pflegerPane = new AnchorPane();
+
         Tab neuesTier = new Tab("Neues Tier", neuesTierPane);
         Tab tiere = new Tab("Tiere", tierePane);
         Tab neuerPfleger = new Tab("Neuer Pfleger", neuerPflegerPane);
