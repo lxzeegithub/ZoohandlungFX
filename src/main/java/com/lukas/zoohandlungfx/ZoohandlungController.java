67eb9e86186f56ddb0f74ac53c41c6d99b90999d
lukas <lukas.koch-schulte@gmx.de>
2023-09-18 12:54:48 +0200

    intital push

--- src/main/java/com/lukas/zoohandlungfx/ZoohandlungController.java
+++ src/main/java/com/lukas/zoohandlungfx/ZoohandlungController.java
@@ -297,8 +297,37 @@ public class ZoohandlungController implements Initializable {
         //Pfleger
         AnchorPane pflegerPane = new AnchorPane();
         Label titlePfleger = createTitel("Pfleger");
+        ComboBox<String> searchDropDownPfleger = createDropdown("Suche", 30, 50, 100, 20, "Name", "Alter", "Tierart", "Rasse", "Preis");
+        TextField searchBarPfleger = createTextField("Suchen", 150, 50, 360, 20, false);
+        TableView<String> tablePfleger = new TableView<>();
+        tablePfleger.setEditable(true);
+        tablePfleger.setPrefWidth(480);
+        tablePfleger.setPrefHeight(300);
+        tablePfleger.setTranslateX(30);
+        tablePfleger.setTranslateY(80);
+        TableColumn nameColumnPfleger = new TableColumn("Name");
+        nameColumnPfleger.setReorderable(false);
+        nameColumnPfleger.setResizable(false);
+        nameColumnPfleger.setPrefWidth(150);
+        TableColumn alterColumnPfleger = new TableColumn("Alter");
+        alterColumnPfleger.setReorderable(false);
+        alterColumnPfleger.setResizable(false);
+        alterColumnPfleger.setPrefWidth(100);
+        TableColumn geschlechtColumnPfleger = new TableColumn("Geschlecht");
+        geschlechtColumnPfleger.setReorderable(false);
+        geschlechtColumnPfleger.setResizable(false);
+        geschlechtColumnPfleger.setEditable(false);
+        geschlechtColumnPfleger.setPrefWidth(120);
+        TableColumn gehaltColumnPfleger = new TableColumn("Gehalt");
+        gehaltColumnPfleger.setReorderable(false);
+        gehaltColumnPfleger.setResizable(false);
+        gehaltColumnPfleger.setPrefWidth(110);
+        tablePfleger.getColumns().addAll(nameColumnPfleger, alterColumnPfleger, geschlechtColumnPfleger, gehaltColumnPfleger);
         pflegerPane.getChildren().addAll(
-                titlePfleger
+                titlePfleger,
+                searchDropDownPfleger,
+                searchBarPfleger,
+                tablePfleger
         );
 
 
